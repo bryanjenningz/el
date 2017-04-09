8869184ff0ed28b5361e5a1e00b10aedb494a63f
Bryan Jennings <bryanjenningz@gmail.com>
2017-04-08 17:57:08 -0700

    add support for array of elements children

--- el.js
+++ el.js
@@ -1,20 +1,29 @@
 function el(type, attrs, ...children) {
-  var element = document.createElement(type);
-  for (var attrKey in attrs) {
+  const element = document.createElement(type);
+
+  for (const attrKey in attrs) {
     if (attrKey.indexOf('on') === 0) {
       element[attrKey] = attrs[attrKey];
     } else {
       element.setAttribute(attrKey, attrs[attrKey]);
     }
   }
-  for (var i = 0; i < children.length; i++) {
-    var child = children[i];
+
+  for (let i = 0; i < children.length; i++) {
+    let child = children[i];
     if (['string', 'number'].indexOf(typeof child) >= 0) {
-      var text = child;
+      const text = String(child);
       child = document.createElement('span');
       child.textContent = text;
+      element.appendChild(child);
+    } else if (Array.isArray(child)) {
+      for (let j = 0; j < child.length; j++) {
+        element.appendChild(child[j]);
+      }
+    } else { // then child is an element, so we can just append it
+      element.appendChild(child);
     }
-    element.appendChild(child);
   }
+  
   return element;
 }
